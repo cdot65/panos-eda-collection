5dd3db6e74e14fb43f26d8e90a44f6b638bcb04e
Calvin Remsburg <cremsburg.dev@gmail.com>
2023-04-06 10:25:54 -0500

    docs

--- plugins/event_source/logs.py
+++ plugins/event_source/logs.py
@@ -15,64 +15,90 @@ Example:
         host: 0.0.0.0
         port: 5000
         type: decryption
-
 """
 
 import asyncio
+import logging
 from typing import Any, Dict
 
 from aiohttp import web
 from dpath import util
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 routes = web.RouteTableDef()
 
 
 @routes.get("/")
 async def status(request: web.Request):
+    """Return a simple status response."""
     return web.Response(status=200, text="up")
 
 
 @routes.post("/{endpoint}")
 async def webhook(request: web.Request):
-    payload = await request.json()
-    endpoint = request.match_info["endpoint"]
+    """
+    Handle webhook requests.
+
+    Process the incoming JSON payload and forward it to the event queue
+    if it matches the configured log type.
+    """
+    try:
+        payload = await request.json()
+    except Exception as e:
+        logger.error(f"Failed to parse JSON payload: {e}")
+        return web.Response(status=400, text="Invalid JSON payload")
 
     if request.app["type"] == "decryption":
-        try:
-            error = payload["type"]
-            if error == "decryption":
-                log_type = "decryption"
-            else:
-                log_type = "log_type"
+        log_type = payload.get("type", "log_type")
+        if log_type != "decryption":
+            log_type = "log_type"
 
-            device_name = util.get(payload, "details.device_name", separator=".")
-            data = {
-                "payload": payload,
-                "meta": {
-                    "device_name": device_name,
-                    "endpoint": endpoint,
-                    "headers": dict(request.headers),
-                    "log_type": log_type,
-                },
-            }
-            await request.app["queue"].put(data)
-
-        except KeyError:
-            data = {
-                "payload": payload,
-                "meta": {
-                    "message": "processing failed, check key names",
-                    "headers": dict(request.headers),
-                },
-            }
-            await request.app["queue"].put(data)
+        data = process_payload(request, payload, log_type)
+        await request.app["queue"].put(data)
 
     return web.Response(
         status=202, text=str({"status": "received", "payload": "happy"})
     )
 
 
-async def main(queue: asyncio.Queue, args: Dict[str, Any]):
+def process_payload(request, payload, log_type):
+    """
+    Process the payload and extract the necessary information.
+
+    :param request: The incoming webhook request.
+    :param payload: The JSON payload from the request.
+    :param log_type: The log type to filter events.
+    :return: A dictionary containing the processed payload and metadata.
+    """
+    try:
+        device_name = util.get(payload, "details.device_name", separator=".")
+        data = {
+            "payload": payload,
+            "meta": {
+                "device_name": device_name,
+                "endpoint": request.match_info["endpoint"],
+                "headers": dict(request.headers),
+                "log_type": log_type,
+            },
+        }
+    except KeyError:
+        logger.warning("KeyError occurred while processing the payload")
+        data = {
+            "payload": payload,
+            "meta": {
+                "message": "processing failed, check key names",
+                "headers": dict(request.headers),
+            },
+        }
+    return data
+
+
+async def main(queue: asyncio.Queue, args: Dict[str, Any], logger=None):
+    if logger is None:
+        logger = logging.getLogger(__name__)
+
     app = web.Application()
     app["queue"] = queue
     app["type"] = str(args.get("type", "decryption"))
@@ -87,7 +113,7 @@ async def main(queue: asyncio.Queue, args: Dict[str, Any]):
     try:
         await asyncio.Future()
     except asyncio.CancelledError:
-        print("Plugin Task Cancelled")
+        logger.info("Plugin Task Cancelled")
     finally:
         await runner.cleanup()
 
